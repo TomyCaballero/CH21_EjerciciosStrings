92099fdd5848687428c7a7bc6f2aa89760a9f6b1
TomyCaballero <tomyC80@gmail.com>
2022-12-12 15:43:41 -0600

    Se agrego la silución 2 del ejercicio 2

--- js/main.js
+++ js/main.js
@@ -24,24 +24,35 @@ for (let index = 0; index < nombre.length; index++) {
 
 console.log(letra, contFinal);
 
-//Entrada 
-"Lura Aline"
-//Salida
-"enilA aruaL"
+// //Entrada 
+// //"Lura Aline"
+// //Salida
+// //"enilA aruaL"
+ 
+//Solución 1
+// nombre = "Ricardo Arturo";
 
-nombre = "Ricardo Arturo";
+// //Se define la variable reversa
+// let reversa="";
 
-//Se define la variable reversa
-let reversa="";
+// //Se crea el ciclo for donde la recorre letra por letra 
+// for (let index = nombre.length; index > -1; index--){
+//     console.log(nombre.charAt(index));
 
-//Se crea el ciclo for donde la recorre letra por letra 
-for (let index = nombre.length; index > -1; index--){
-    console.log(nombre.charAt(index));
-
-    reversa= reversa + nombre.charAt(index);
+//     reversa = reversa + nombre.charAt(index);
    
-}// for index
+// }// for index
 
-console.log(reversa.charAt);
+// console.log(reversa.charAt);
 
+
+//Solución 2
+function reves (str) {
+    let tmpStr="";
+    for (let indice = str.length - 1; indice >= 0; indice--) {
+        tmpStr += str.charAt(indice);
+        
+    } // for indice
+    return tmpStr;
+}
   
