4986f248161c0d9c62971b09c163a348afc0c0de
TomyCaballero <tomyC80@gmail.com>
2022-12-12 17:31:43 -0600

    Se agrego la rama validaciones con un formulario y validación de correo y texto

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+let btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+    let emailgRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML="";
+
+    if (exampleFormControlTextarea1.value.trim().replaceAll("  " , "").length < 20) {
+        alertError.innerHTML= "El mensaje debe contener 20 caracteres o más"
+        alertError.style.display="block";
+        exampleFormControlTextarea1.focus();
+        exampleFormControlTextarea1.select();
+    }//if
+
+    if (exampleFormControlInput1.value.math(emailgRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electronico no es válido.";
+    }
+});
